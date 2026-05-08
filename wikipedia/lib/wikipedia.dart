a29230c815c7bed7589c6e326f9614ff3f04adfe
kendra <kendra@kendralee.com>
2026-05-08 10:12:16 -0400

    initial commit to GitHub

--- wikipedia/lib/wikipedia.dart
+++ wikipedia/lib/wikipedia.dart
@@ -5,8 +5,3 @@ export 'src/model/article.dart';
 export 'src/model/search_results.dart';
 export 'src/model/summary.dart';
 export 'src/model/title_set.dart';
-
-
-int calculate() {
-  return 6 * 7;
-}
